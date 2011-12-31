ce44650819003a6dc2e0e85598b40382f141fbf9
Joe Bain <mrjoebain@gmail.com>
2011-12-31 20:03:41 +0000

    drawing the depth buffer on top of the scene for whatever reason

--- spheres/spheres.js
+++ spheres/spheres.js
@@ -3,110 +3,142 @@ var camera, scene, renderer;
 var spheres = [];
 var planes = [];
 
-var renderTexture, screenScene, screenCamera, screen;
+var renderTexture, depthTexture, screenScene, screenCamera, screen, depthScreen;
 
-var aaScale = 2;
+var aaScale = 2, depthTextureScale = 2;
 
-	window.onload = function() {
-		init();
-		animate();
-	};
+var depthMaterial, redPhongMaterial, bluePhongMaterial;
 
-    function init() {
+window.onload = function() {
+	init();
+	animate();
+};
 
-        scene = new THREE.Scene();
+function init() {
 
-        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 10000 );
-        camera.position.z = 300;
-        scene.add( camera );
+	scene = new THREE.Scene();
 
-		for (var i = 0 ; i < 100 ; i++) {
-			spheres[i] = {};
-			var sphere = spheres[i];
-			sphere.geometry = new THREE.SphereGeometry( 10, 20, 20 );
-			sphere.material = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 10000 );
+	camera.position.z = 300;
+	scene.add( camera );
 
-			sphere.mesh = new THREE.Mesh( sphere.geometry, sphere.material );
-			scene.add( sphere.mesh );
-			sphere.mesh.position.x = (Math.random() - 0.5)*200;
-			sphere.mesh.position.y = (Math.random() - 0.5)*200;
-			sphere.mesh.position.z = (Math.random() - 0.5)*200;
-		}
+	// depth writer material
+	var depthFragmentShader = document.getElementById("ssaoFrag").innerHTML;
+	var depthVertexShader = document.getElementById("plainVert").innerHTML;
+	var depthUniforms = THREE.UniformsUtils.merge([
+			THREE.UniformsLib[ "common" ],
+			{mNear:{type:"f", value:0}, mFar:{type:"f", value:1000}, opacity:{type:"f", value: 1}}
+			]);
 
-		for (var i = 0 ; i < 5 ; i++) {
-			planes[i] = {};
-			var plane = planes[i];
-			
-			plane.geometry = new THREE.CubeGeometry( 1, 100, 100 );
-			plane.material = new THREE.MeshPhongMaterial( { color: 0x0000ff } );
+	var depthParameters = { fragmentShader: depthFragmentShader, vertexShader: depthVertexShader, uniforms: depthUniforms };
+	depthMaterial = new THREE.ShaderMaterial( depthParameters );
 
-			plane.mesh = new THREE.Mesh( plane.geometry, plane.material );
-			scene.add( plane.mesh );
-			plane.mesh.position.x = (Math.random() - 0.5)*200;
-			plane.mesh.position.y = (Math.random() - 0.5)*200;
-			plane.mesh.position.z = (Math.random() - 0.5)*200;
-			plane.mesh.rotation.x = (Math.random() - 0.5)*Math.PI*2;
-			plane.mesh.rotation.y = (Math.random() - 0.5)*Math.PI*2;
-			plane.mesh.rotation.z = (Math.random() - 0.5)*Math.PI*2;
+	redPhongMaterial = new THREE.MeshPhongMaterial( { color: 0xff0000 } );	
+	bluePhongMaterial = new THREE.MeshPhongMaterial( { color: 0x0000ff } );	
 
-		}
 
-		// create a point light
-		var pointLight = new THREE.PointLight( 0xFFFFFF );
+	for (var i = 0 ; i < 100 ; i++) {
+		spheres[i] = {};
+		var sphere = spheres[i];
+		sphere.geometry = new THREE.SphereGeometry( 10, 20, 20 );
+		sphere.material = redPhongMaterial;
 
-		// set its position
-		pointLight.position.x = 10;
-		pointLight.position.y = 50;
-		pointLight.position.z = 130;
+		sphere.mesh = new THREE.Mesh( sphere.geometry, sphere.material );
+		scene.add( sphere.mesh );
+		sphere.mesh.position.x = (Math.random() - 0.5)*200;
+		sphere.mesh.position.y = (Math.random() - 0.5)*200;
+		sphere.mesh.position.z = (Math.random() - 0.5)*200;
+	}
 
-		// add to the scene
-		scene.add(pointLight);
+	for (var i = 0 ; i < 5 ; i++) {
+		planes[i] = {};
+		var plane = planes[i];
 
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-		//renderer.setClearColorHex(0x333333);
+		plane.geometry = new THREE.CubeGeometry( 1, 100, 100 );
+		plane.material = bluePhongMaterial;
 
-		renderTexture = new THREE.WebGLRenderTarget( window.innerWidth*aaScale, window.innerHeight*aaScale, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter } );
+		plane.mesh = new THREE.Mesh( plane.geometry, plane.material );
+		scene.add( plane.mesh );
+		plane.mesh.position.x = (Math.random() - 0.5)*200;
+		plane.mesh.position.y = (Math.random() - 0.5)*200;
+		plane.mesh.position.z = (Math.random() - 0.5)*200;
+		plane.mesh.rotation.x = (Math.random() - 0.5)*Math.PI*2;
+		plane.mesh.rotation.y = (Math.random() - 0.5)*Math.PI*2;
+		plane.mesh.rotation.z = (Math.random() - 0.5)*Math.PI*2;
 
-		screenCamera = new THREE.Camera();
-		screenCamera.projectionMatrix = THREE.Matrix4.makeOrtho( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, -10000, 10000 );
-		screenCamera.position.z = 100;
+	}
 
-		screenScene = new THREE.Scene();
+	// create a point light
+	var pointLight = new THREE.PointLight( 0xFFFFFF );
 
-		screen = {};
-		screen.geometry = new THREE.PlaneGeometry( window.innerWidth, window.innerHeight );
-		screen.material = new THREE.MeshBasicMaterial( {map:renderTexture} );
+	// set its position
+	pointLight.position.x = 10;
+	pointLight.position.y = 50;
+	pointLight.position.z = 130;
 
-		screen.mesh = new THREE.Mesh( screen.geometry, screen.material );
-		screen.mesh.position.z = -100;
-		screenScene.add( screen.mesh );
-		screenScene.add( screenCamera );
+	// add to the scene
+	scene.add(pointLight);
 
-        document.body.appendChild( renderer.domElement );
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	//renderer.setClearColorHex(0x333333);
 
-    }
+	renderTexture = new THREE.WebGLRenderTarget( window.innerWidth*aaScale, window.innerHeight*aaScale, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter } );
+	depthTexture = new THREE.WebGLRenderTarget( window.innerWidth/depthTextureScale, window.innerHeight/depthTextureScale, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter } );
 
-    function animate() {
+	screenCamera = new THREE.Camera();
+	screenCamera.projectionMatrix = THREE.Matrix4.makeOrtho( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, -10000, 10000 );
+	screenCamera.position.z = 100;
 
-		var rotation = new Date() / 10000;
-		var radius = 200;
-		camera.position.x = Math.sin(rotation)*radius;
-		camera.position.z = Math.cos(rotation)*radius;
+	screenScene = new THREE.Scene();
 
-		camera.lookAt({x:0, y:0, z:0});
+	screen = {};
+	screen.geometry = new THREE.PlaneGeometry( window.innerWidth, window.innerHeight );
+	screen.material = new THREE.MeshBasicMaterial( {map:renderTexture} );
+	screen.mesh = new THREE.Mesh( screen.geometry, screen.material );
+	screen.mesh.position.z = -100;
 
-        requestAnimationFrame( animate );
-        render();
+	depthScreen = {};
+	depthScreen.geometry = new THREE.PlaneGeometry( window.innerWidth, window.innerHeight );
+	depthScreen.material = new THREE.MeshBasicMaterial( {map:depthTexture, opacity:0.5, transparent:true} );
+	depthScreen.mesh = new THREE.Mesh( depthScreen.geometry, depthScreen.material );
+	depthScreen.mesh.position.z = -90;
 
-    }
+	screenScene.add( screen.mesh );
+	screenScene.add( depthScreen.mesh );
+	screenScene.add( screenCamera );
 
-    function render() {
+	document.body.appendChild( renderer.domElement );
 
-        renderer.setViewport(0,0, window.innerWidth*aaScale, window.innerHeight*aaScale );
-        renderer.render( scene, camera, renderTexture );
+}
 
-        renderer.setViewport(0,0, window.innerWidth, window.innerHeight );
-        renderer.render( screenScene, screenCamera );
+function animate() {
 
-    }
+	var rotation = new Date() / 10000;
+	var radius = 200;
+	camera.position.x = Math.sin(rotation)*radius;
+	camera.position.z = Math.cos(rotation)*radius;
+
+	camera.lookAt({x:0, y:0, z:0});
+
+	requestAnimationFrame( animate );
+	render();
+
+}
+
+function render() {
+	// render the depth texture
+	scene.overrideMaterial = depthMaterial;
+	renderer.setViewport(0,0, window.innerWidth/depthTextureScale, window.innerHeight/depthTextureScale );
+	renderer.render( scene, camera, depthTexture );
+
+	// render the scene normally
+	scene.overrideMaterial = undefined;
+	renderer.setViewport(0,0, window.innerWidth*aaScale, window.innerHeight*aaScale );
+	renderer.render( scene, camera, renderTexture );
+
+	// render the final output
+	renderer.setViewport(0,0, window.innerWidth, window.innerHeight );
+	renderer.render( screenScene, screenCamera );
+
+}
